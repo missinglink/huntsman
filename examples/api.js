99cfc1f7f6cde37492db04d385ff19a86f79b875
Peter Johnson <peter@openshuffle.com>
2013-07-19 20:52:14 +0100

    better api example

--- examples/api.js
+++ examples/api.js
@@ -3,38 +3,32 @@
 
 var huntsman = require('huntsman');
 var spider = huntsman.spider({
-  throttle: ( 1 / 60 ) // unauthenticated clients can only make up to 60 requests per hour
+  throttle: ( 6 / 60 ) // max 6 request per min (unauthenticated clients can only make up to 60 requests per hour)
 });
 
 spider.extensions = [
   huntsman.extension( 'recurse', {  // load recurse extension & follow 'followers' links
     pattern: {
       search: /"followers_url"\s?:\s?"([^"]+)/gi, // extract 'followers' links
-      refine: /"followers_url"\s?:\s?"([^"#]+)/
+      refine: /"followers_url"\s?:\s?"([^"#]+)/i
     }
-  })
+  }),
+  huntsman.extension( 'ratelimit' ), // handle github api rate limiting
+  huntsman.extension( 'json' ), // parse json
+  huntsman.extension( 'stats' ) // load stats extension
 ];
 
-// detect when we get rate limited
-spider.on( 'response', function( err, res ){
-  if( res && res.headers && res.headers[ 'x-ratelimit-remaining' ] == '0' ){
-    console.log( 'rate limited', res.headers[ 'x-ratelimit-remaining' ] + '/' + res.headers[ 'x-ratelimit-limit' ] );
-    console.log( 'rate limit retored at ' + new Date( parseInt( res.headers[ 'x-ratelimit-reset' ], 10 ) * 1000 ) );
-  }
-});
-
-// find followers pages
+// find all followers recursively
 spider.on( /followers$/, function ( err, res ){
 
-  var json = JSON.parse( res.body );
-
-  // print http response
   console.log( res.uri );
-  json.forEach( function( user ){
-    console.log( user.id, user.login );
-  });
+
+  // print response
+  var json = res.extension.json;
+  if( json ) console.log( json );
 
 });
 
-spider.queue.add( 'https://api.github.com/users/missinglink' );
+spider.queue.add( 'https://api.github.com/users/missinglink/followers' );
 spider.start();
+console.log( 'DEMO: refreshing ' + ( spider.options.throttle * 60 ) + ' times per minute' );
